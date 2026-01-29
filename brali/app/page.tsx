66149e51c9b034a48af656daa02ef201fe8bc19d
JersonNetcol <j.espitia@netcol.net.co>
2026-01-29 15:13:55 -0500

    fff

--- brali/app/page.tsx
+++ brali/app/page.tsx
@@ -1,5 +1,5 @@
-import Header from "../components/header";
-import Hero from "../components/Hero";
+import Header from "./components/header";
+import Hero from "./components/Hero";
 
 
 export default function Home() {
